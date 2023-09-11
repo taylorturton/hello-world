0abbc19ffd9cbeb0ea9ab622ef8f6e564469ffd3
taylorturton <126579693+taylorturton@users.noreply.github.com>
2023-09-11 14:29:55 +0100

    Add files via upload

--- character_count.py
+++ character_count.py
@@ -3,9 +3,12 @@ character_count = 0
 consonant_count = 0
 vowel_count = 0
 letter_count = 0
+word_count = 0
 
 consonants = 'bcdfghjklmnpqrstvwxyz'
 vowels = 'aeiou'
+
+words = text.split()
 
 for char in text:
     character_count += 1
@@ -16,7 +19,10 @@ for char in text:
         vowel_count += 1
         letter_count += 1
 
+word_count = len(words)
+
 print("Total characters: ", character_count)
 print("Total letters: ", letter_count)
 print("Total consonants: ", consonant_count)
 print("Total vowels: ", vowel_count)
+print("Total words: ", word_count)
