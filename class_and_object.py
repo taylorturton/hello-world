f44dd55d189c6b639ccf51f5d12bd74c0d62c799
taylorturton <126579693+taylorturton@users.noreply.github.com>
2023-09-11 10:58:50 +0100

    Add files via upload

---
+++ class_and_object.py
@@ -0,0 +1,9 @@
+class CookieCutter:
+    def __init__(self, chocolate_chips):
+        self.chocolate_chips = chocolate_chips
+
+Cookie1 = CookieCutter(5)
+print("Cookie 1 has " + str(Cookie1.chocolate_chips) + " chocolate chips")
+
+Cookie2 = CookieCutter(12)
+print("Cookie 2 has " + str(Cookie2.chocolate_chips) + " chocolate chips")
